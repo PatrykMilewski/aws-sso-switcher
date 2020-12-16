fb23215803acfcb1ef05ff1544eab36a3c08e315
kkrampa <kamilkrampa87@gmail.com>
2020-12-16 12:48:17 +0100

    Remove unused code

--- credential_process.sh
+++ credential_process.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# unset vars because will be run in parent shell context using source command
-unset AWS_ACCESS_KEY_ID
-unset AWS_SECRET_ACCESS_KEY
-unset AWS_SESSION_TOKEN
 unset profile
 unset role_arn
 unset source_profile
@@ -26,18 +22,6 @@ role_arn=$(aws configure get ${profile}.role_arn)
 source_profile=$(aws configure get ${profile}.source_profile)
 
 creds=$(aws sts assume-role --profile ${source_profile} --role-arn ${role_arn} --role-session-name console-temp-role 2>&1)
-return_code=$?
-if [ ! $return_code -eq 0 ] 
-then
-   if echo $creds | grep "expired"; then
-    aws sso login --profile $source_profile
-    creds=$(aws sts assume-role --profile ${source_profile} --role-arn ${role_arn} --role-session-name console-temp-role 2>&1)
-   fi
-fi
-
-export AWS_ACCESS_KEY_ID=$(echo $creds | jq .Credentials.AccessKeyId -r) 
-export AWS_SECRET_ACCESS_KEY=$(echo $creds  | jq .Credentials.SecretAccessKey -r) 
-export AWS_SESSION_TOKEN=$(echo $creds  | jq .Credentials.SessionToken -r)
 
 echo $creds | jq '.Credentials + {Version: 1}' -r
 
