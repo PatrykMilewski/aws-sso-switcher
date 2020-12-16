3e09e9f6d044a78705fe4d99706d69b20e67e455
kkrampa <kamilkrampa87@gmail.com>
2020-12-16 12:51:37 +0100

    Open browser when not login

--- credential_process.sh
+++ credential_process.sh
@@ -22,6 +22,14 @@ role_arn=$(aws configure get ${profile}.role_arn)
 source_profile=$(aws configure get ${profile}.source_profile)
 
 creds=$(aws sts assume-role --profile ${source_profile} --role-arn ${role_arn} --role-session-name console-temp-role 2>&1)
+return_code=$?
+if [ ! $return_code -eq 0 ]
+then
+   if echo $creds | grep "expired"; then
+    aws sso login --profile $source_profile
+    creds=$(aws sts assume-role --profile ${source_profile} --role-arn ${role_arn} --role-session-name console-temp-role 2>&1)
+   fi
+fi
 
 echo $creds | jq '.Credentials + {Version: 1}' -r
 
